826eb92e8ae674b95d62d2b5ec31196d17279b0b
MOHAMED MAHER <141060515+MohamedMaherTawfik@users.noreply.github.com>
2024-02-27 07:03:22 +0200

    add edit & Delete Post

--- blog/forms.py
+++ blog/forms.py
@@ -4,7 +4,7 @@ from .models import Post
 class PostForm(forms.ModelForm):
     class Meta:
         model=Post
-        fields='__all__'
-        
+        # fields='__all__'
+        exclude=('author',)
         
 
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Post
 from .forms import PostForm
 
@@ -16,9 +16,30 @@ def new_post(request):
     if request.method=='POST':
         form=PostForm(request.POST,request.FILES)
         if form.is_valid():
-            form.save()
+            myform=form.save(commit=False)
+            myform=request.user
+            myform.save()
+            return redirect('/blog')
     else:
-        form=PostForm
-    form=PostForm
+        form=PostForm()
     return render(request,'new_post.html',{'form':form})
 
+def edit_post(request,post_id):
+    data=Post.objects.get(id=post_id)
+    if request.method=='POST':
+        form=PostForm(request.POST,request.FILES,instance=data)
+        if form.is_valid():
+            myform=form.save(commit=False)
+            myform=request.user
+            myform.save()
+            return redirect('/blog')
+    else:
+        form=PostForm(instance=data)
+    
+    return render(request,'new_post.html',{'form':form})
+
+
+def delete_post(request,post_id):
+    data=Post.objects.get(id=post_id)
+    data.delete()
+    return redirect('/blog')
